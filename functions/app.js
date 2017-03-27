42cf716eab5b30c7878c64602c53a2897734b9fc
Grégory <claudejurado@orange.fr>
2017-03-27 14:43:52 +0200

    SAns tous les problemes de plantage du portable, j'aurais commit plus souvent, donc exercices 1, 2, 3, 4

---
+++ functions/app.js
@@ -0,0 +1,33 @@
+function alwaysTrue (){
+		return true;
+	};
+
+
+function string(yipikai) {
+	return yipikai;
+};
+
+
+function concat(a,b){
+
+	return (a + b);
+
+}
+console.log(concat("yabada","dou"));
+
+function comparate(x,y){
+	if (x>y){
+		return "le premier nombre est plus grand"
+	}
+	else if (x<y){
+		return "le premier nombre est plus petit"
+	}	
+ else {
+	return "les deux nombres sont identiques"
+}
+};
+
+console.log(comparate(6,3));
+console.log(comparate(3,6));
+console.log(comparate(6,6));
+
