a1032f994f53e2f34f890e62159cc18a8d7c2f0f
Grégory <claudejurado@orange.fr>
2017-03-27 11:06:24 +0200

    exercice 5

--- array/app.js
+++ array/app.js
@@ -2,3 +2,6 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","dececmbre"];
  console.log(month[2]);
  console.log(month[5]);
+ console.log(month[10]);
+ var modif = month.splice(7,1,"août");
+ console.log(month);
