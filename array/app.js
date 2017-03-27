4e5cd7cd9283c2c28d72006ad3ffe5822cdf8bed
Grégory <claudejurado@orange.fr>
2017-03-27 11:11:20 +0200

    exercice 6

--- array/app.js
+++ array/app.js
@@ -4,4 +4,4 @@ var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","s
  console.log(month[5]);
  console.log(month[10]);
  var modif = month.splice(7,1,"août");
- console.log(month);
+ console.log(month ); 
